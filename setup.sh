fb0c5fca5a08bc7783fb1191c0cce72cc236df2c
Klaasjan <klaasjan@capteur.nl>
2022-05-30 15:41:46 +0200

    Setup

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
-# build image and import it using a local tarball
-docker build ./sine-source -t rug_node:local
+docker build . -t rug_node:local
 docker save rug_node:local > rug_node.tar
-microk8s ctr image import rug_node.tar
 
-# configure k8s pod with our application and related servicemonitor
-microk8s kubectl apply -f application.yaml 
+microk8s ctr image import rug_node.tar
+microk8s kubectl apply -f deployment.yaml 
 microk8s kubectl apply -f service.yaml 
 microk8s kubectl apply -f serviceMonitor.yaml 
+microk8s kubectl apply -f createCustomResource.yaml 
+microk8s kubectl apply -f customResource.yaml 
+
 
 
 #microk8s ctr images rm docker.io/library/rug_node:local
